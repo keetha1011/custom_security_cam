3e1d3873a6957ea34d9b58d6442c5e89ed625747
Keerthan <136081248+keetha1011@users.noreply.github.com>
2024-03-11 16:33:52 +0530

    Gallery improved a lot with caching; Features added: delete, save, and share the images...

--- lib/main.dart
+++ lib/main.dart
@@ -1,99 +1,48 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
-
 import 'package:firebase_core/firebase_core.dart';
-
 import 'package:google_fonts/google_fonts.dart';
-
 import 'package:firebase_auth/firebase_auth.dart';
-
 import 'package:connectivity_plus/connectivity_plus.dart';
-
-import 'package:permission_handler/permission_handler.dart';
-
 import 'firebase_options.dart';
-
 import 'pages/login_page.dart';
-
 import 'pages/home_page.dart';
 
 
 
-
-
-
-
 Future<void> main() async {
-
   WidgetsFlutterBinding.ensureInitialized();
-
   var connectivityResult = await Connectivity().checkConnectivity();
-
   if (connectivityResult == ConnectivityResult.none) {
-
     debugPrint("Connection Error");
-
     const SnackBar(
-
       content: Text("Connection Error, Try Again"),
-
     );
-
-    Permission.photos;
-
     return;
-
   }
-
   await Firebase.initializeApp(
-
     options: DefaultFirebaseOptions.currentPlatform,
-
   );
 
-
-
   runApp(const MyApp());
-
 }
 
-
-
 class MyApp extends StatelessWidget {
-
   //final User? user;
-
   //const MyApp({Key? key, this.user}) : super(key: key);
-
   const MyApp({super.key});
 
-
-
   @override
-
   Widget build(BuildContext context) {
-
     return MaterialApp(
-
       debugShowCheckedModeBanner: false,
-
       theme: ThemeData(
-
         useMaterial3: false,
-
         fontFamily: GoogleFonts.dmSans().fontFamily,
-
         primarySwatch: Colors.grey,
-
       ),
-
       home: FirebaseAuth.instance.currentUser?.email != null ? const HomePage() : LoginPage() ,
 
-
-
     );
-
   }
-
 }
--- lib/pages/captures_view.dart
+++ lib/pages/captures_view.dart
@@ -18,7 +18,7 @@ class _CapturesGalleryState extends State<CapturesGallery> {
         children: [
           const PageHeaderText(pageHeader: "Gallery", pageSubHeader: "Recent Captures by the Camera",),
           const SizedBox(width: 25,height: 25,),
-          DownloadAndDisplayImages(userEmail: FirebaseAuth.instance.currentUser?.email ?? ""),
+          DownloadAndDisplayImages(userEmail: "nnm23am025@nmamit.in",),
         ],
       ),
     );
